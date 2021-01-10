2ed8715277700e20db9922a6241634b805290fdc
post45 <bashkuresh92@yandex.ru>
2021-01-10 09:34:13 -0500

    Procfile

--- src/main/java/com/anabiullin/ppmtool3/web/UserController.java
+++ src/main/java/com/anabiullin/ppmtool3/web/UserController.java
@@ -69,3 +69,6 @@ public class UserController {
       return new ResponseEntity<User>(newUser, HttpStatus.CREATED);
     }
 }
+
+//original  web: java $JAVA_OPTS -jar target/ppmtool3-0.0.1-SNAPSHOT.jar -Dserver.port=$PORT $JAR_OPTS
+//web: java -Dserver.port=$PORT $JAVA_OPTS -jar target/ppmtool3-0.0.1-SNAPSHOT.jar
